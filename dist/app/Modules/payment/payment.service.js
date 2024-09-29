f29a1838bee93fc7e51f7aed60ba9642986e823b
AkashAkter <akash31akter@gmail.com>
2024-09-29 23:54:22 +0600

    DONE

--- dist/app/Modules/payment/payment.service.js
+++ dist/app/Modules/payment/payment.service.js
@@ -15,23 +15,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.paymentService = exports.failedPayment = exports.createPayment = void 0;
 const fs_1 = require("fs");
 const path_1 = require("path");
-const payment_model_1 = __importDefault(require("./payment.model"));
 const booking_model_1 = __importDefault(require("../booking/booking.model"));
 const slot_model_1 = __importDefault(require("../slot/slot.model"));
-// import { IPaymentPayload } from "./payment.interface";
-// import axios from "axios";
-// import jwt from "jsonwebtoken";
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const payment_model_1 = __importDefault(require("./payment.model"));
 const createPayment = (amount, transactionId) => __awaiter(void 0, void 0, void 0, function* () {
     yield payment_model_1.default.create({
-        amount,
+        amount: amount,
         transactionId,
         status: "Paid",
     });
     const filePath = (0, path_1.join)(__dirname, "../../templates/success.html");
     let file = (0, fs_1.readFileSync)(filePath, "utf-8");
-    file = file.replace("{{link}}", "https://auto-limpio-server.vercel.app/");
+    file = file.replace("{{link}}", "http://localhost:5000/");
     return file;
 });
 exports.createPayment = createPayment;
@@ -40,7 +35,7 @@ const failedPayment = (slot) => __awaiter(void 0, void 0, void 0, function* () {
     yield slot_model_1.default.findOneAndUpdate({ _id: slot }, { $set: { isBooked: "available" } });
     const filePath = (0, path_1.join)(__dirname, "../../templates/error.html");
     let file = (0, fs_1.readFileSync)(filePath, "utf-8");
-    file = file.replace("{{link}}", "https://auto-limpio-server.vercel.app/");
+    file = file.replace("{{link}}", "http://localhost:5000/");
     return file;
 });
 exports.failedPayment = failedPayment;
